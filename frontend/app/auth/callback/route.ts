864dfd99a6424e08688c15a7b8858719e4cbdf93
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2026-03-15 18:06:22 -0300

    feature: UI UX de todas telas auth melhorada, login social com google

--- frontend/app/auth/callback/route.ts
+++ frontend/app/auth/callback/route.ts
@@ -6,6 +6,8 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
 
+  console.log('teste')
+
   if (!code) {
     return NextResponse.redirect(`${origin}/auth`);
   }
